4db2001cc9b478d9ebef3cee17869753ff6503da
Oliver Alvarado Rodriguez <oliver.alvarado-rodriguez@hpe.com>
2025-10-28 13:12:06 -0500

    code formatting changes
    Signed-off-by: Oliver Alvarado Rodriguez <oliver.alvarado-rodriguez@hpe.com>

--- phold/Node.h
+++ phold/Node.h
@@ -11,75 +11,86 @@
 #endif
 
 class Node : public SST::Component {
-  public:
-    Node( SST::ComponentId_t id, SST::Params& params );
+public:
+    Node(SST::ComponentId_t id, SST::Params& params);
     ~Node();
 
     void setup() override;
     void finish() override;
 
-    bool tick( SST::Cycle_t currentCycle );
+    bool tick(SST::Cycle_t currentCycle);
 
-    void handleEvent(SST::Event *ev);
+    void handleEvent(SST::Event* ev);
 
-    SST::Interfaces::StringEvent * createEvent();
+    SST::Interfaces::StringEvent* createEvent();
 
     virtual size_t movementFunction();
     virtual SST::SimTime_t timestepIncrementFunction();
 
-  #ifdef ENABLE_SSTCHECKPOINT
+#ifdef ENABLE_SSTCHECKPOINT
     // Serialization support for checkpointing/restart
-    void serialize_order(SST::Core::Serialization::serializer& ser) override;
-    // Default constructor for checkpointing - initialize critical members
+    void serialize_order(
+        SST::Core::Serialization::serializer& ser) override;
+    // Default constructor for checkpointing - initialize members
     Node() : rng(nullptr), numLinks(0) {}
-  #endif
+#endif
 
     // Register the component
     SST_ELI_REGISTER_COMPONENT(
-      Node,   // class
-      "phold",   // element library
-      "Node", // component
-      SST_ELI_ELEMENT_VERSION( 1, 0, 0 ),
-      "Base component for PHOLD benchmark. Each component sends messages to neighbors using the movement and timestep increment functions.",
-      COMPONENT_CATEGORY_UNCATEGORIZED
-    )
+        Node,                   // class
+        "phold",              // element library
+        "Node",               // component
+        SST_ELI_ELEMENT_VERSION(1, 0, 0),
+        "Base component for PHOLD benchmark. Each component sends "
+        "messages to neighbors using the movement and timestep "
+        "increment functions.",
+        COMPONENT_CATEGORY_UNCATEGORIZED)
 
     // Parameter name, description, default value
     SST_ELI_DOCUMENT_PARAMS(
-     { "numRings", "number of rings to connect to", "1" },
-     { "i", "My row index", "-1" },
-     { "j", "My column index", "-1" },
-     { "rowCount", "Total number of rows", "-1"},
-     { "colCount", "Total number of columns", "-1"},
-     { "timeToRun", "Time to run the simulation", "10ns" },
-     { "eventDensity", "Number of events to start with per component", "0.1" },
-     { "smallPayload", "Size of small event payloads in bytes", "8"},
-     { "largePayload", "Size of large event payloads in bytes", "1024"},
-     { "largeEventFraction", "Fraction of events that are large (default: 0.1)", "0.1"},
-     { "verbose", "Whether or not to write the recvCount to file.", "0"},
-     { "componentSize", "Additional size of components in bytes", "0"}
-    )
+        {"numRings", "number of rings to connect to", "1"},
+        {"i", "My row index", "-1"},
+        {"j", "My column index", "-1"},
+        {"rowCount", "Total number of rows", "-1"},
+        {"colCount", "Total number of columns", "-1"},
+        {"timeToRun", "Time to run the simulation", "10ns"},
+        {"eventDensity",
+         "Number of events to start with per component",
+         "0.1"},
+        {"smallPayload",
+         "Size of small event payloads in bytes",
+         "8"},
+        {"largePayload",
+         "Size of large event payloads in bytes",
+         "1024"},
+        {"largeEventFraction",
+         "Fraction of events that are large (default: 0.1)",
+         "0.1"},
+        {"verbose",
+         "Whether or not to write the recvCount to file.",
+         "0"},
+        {"componentSize",
+         "Additional size of components in bytes",
+         "0"})
 
-    SST_ELI_DOCUMENT_PORTS(
-      {"port%d", "Ports to others", {}}
-    )
+    SST_ELI_DOCUMENT_PORTS({{"port%d", "Ports to others", {}}})
 
-    template<typename T>
-    void setupLinks() {
-      //std::cout << "setting up links on rank " << getRank().rank << "...\n" << std::flush;
-      for (int i = 0; i < links.size(); i++) {
-        std::string portName = "port" + std::to_string(i);
-        auto *evHandler = new SST::Event::Handler2<Node, &Node::handleEvent>(this);
-        links[i] = configureLink(portName, evHandler);
-        if (links[i] == nullptr) {
-          //std::cerr << "Failed to configure link " << portName << " on rank " << getRank().rank << " id " << myId << "\n" << std::flush;
+    template <typename T>
+    void setupLinks()
+    {
+        // Configure ports named port0..portN. If configuration fails the
+        // pointer will be nullptr.
+        for (int i = 0; i < static_cast<int>(links.size()); i++) {
+            std::string portName = "port" + std::to_string(i);
+            auto* evHandler = new SST::Event::Handler2<
+                Node, &Node::handleEvent>(this);
+            links[i] = configureLink(portName, evHandler);
+            if (links[i] == nullptr) {
+                // Link not configured; leave nullptr.
+            } else {
+                // Link configured.
+            }
         }
-        else {
-          //std::cout << "Configured link " << portName << " on rank " << getRank().rank << " id " << myId << "\n" << std::flush;
-        }
-      }
-      //std::cout << "link size: " << links.size();
-      //std::cout << "done setting up links on rank " << getRank().rank << "\n" << std::flush;
     }
 
     int myId, myRow, myCol, verbose;
@@ -93,80 +104,89 @@ class Node : public SST::Component {
 
     int recvCount;
 
-    // SST RNG system for proper checkpoint serialization
+    // SST RNG system for checkpoint serialization
     SST::RNG::MersenneRNG* rng;
 
-  #ifdef ENABLE_SSTDBG
+#ifdef ENABLE_SSTDBG
     void printStatus(SST::Output& out) override;
-    SSTDebug *dbg;
-  #endif
+    SSTDebug* dbg;
+#endif
 
-  #ifdef ENABLE_SSTCHECKPOINT
+#ifdef ENABLE_SSTCHECKPOINT
     ImplementSerializable(Node)
-  #endif
+#endif
 };
 
 
 class ExponentialNode : public Node {
-  public:
-  #ifdef ENABLE_SSTCHECKPOINT
-    void serialize_order(SST::Core::Serialization::serializer& ser) override;
+public:
+#ifdef ENABLE_SSTCHECKPOINT
+    void serialize_order(
+        SST::Core::Serialization::serializer& ser) override;
     ExponentialNode() : Node() {}
-  #endif
+#endif
     ExponentialNode(SST::ComponentId_t id, SST::Params& params);
     SST::SimTime_t timestepIncrementFunction() override;
 
     SST_ELI_REGISTER_COMPONENT(
-      ExponentialNode,   // class
-      "phold",   // element library
-      "ExponentialNode", // component
-      SST_ELI_ELEMENT_VERSION( 1, 0, 0 ),
-      "PHOLD node that uses an exponential distribution for its timestep increment function.",
-      COMPONENT_CATEGORY_UNCATEGORIZED
-    )
+        ExponentialNode,      // class
+        "phold",             // element library
+        "ExponentialNode",   // component
+        SST_ELI_ELEMENT_VERSION(1, 0, 0),
+        "PHOLD node that uses an exponential distribution for its "
+        "timestep increment function.",
+        COMPONENT_CATEGORY_UNCATEGORIZED)
 
     // Parameter name, description, default value
     SST_ELI_DOCUMENT_PARAMS(
-     { "multiplier", "Multiplier for exponential distribution, in ns", "1"}
-    )
+        {"multiplier",
+         "Multiplier for exponential distribution, in ns",
+         "1"})
 
     double multiplier;
 
-  #ifdef ENABLE_SSTCHECKPOINT
+#ifdef ENABLE_SSTCHECKPOINT
     ImplementSerializable(ExponentialNode)
-  #endif
+#endif
 };
 
 
 class UniformNode : public Node {
-  public:
-  #ifdef ENABLE_SSTCHECKPOINT
-    void serialize_order(SST::Core::Serialization::serializer& ser) override;
+public:
+#ifdef ENABLE_SSTCHECKPOINT
+    void serialize_order(
+        SST::Core::Serialization::serializer& ser) override;
     UniformNode() : Node() {}
-  #endif
+#endif
     UniformNode(SST::ComponentId_t id, SST::Params& params);
     SST::SimTime_t timestepIncrementFunction() override;
 
     SST_ELI_REGISTER_COMPONENT(
-      UniformNode,   // class
-      "phold",   // element library
-      "UniformNode", // component
-      SST_ELI_ELEMENT_VERSION( 1, 0, 0 ),
-      "PHOLD node that uses a uniform distribution for its timestep increment function.",
-      COMPONENT_CATEGORY_UNCATEGORIZED
-    )
+        UniformNode,          // class
+        "phold",             // element library
+        "UniformNode",       // component
+        SST_ELI_ELEMENT_VERSION(1, 0, 0),
+        "PHOLD node that uses a uniform distribution for its "
+        "timestep increment function.",
+        COMPONENT_CATEGORY_UNCATEGORIZED)
 
     // Parameter name, description, default value
     SST_ELI_DOCUMENT_PARAMS(
-      { "min", "Minimum value for uniform distribution, in ns, in addition to link delay", "0"},
-      { "max", "Maximum value for uniform distribution, in ns, in addition to link delay", "10"}
-    )
+        {"min",
+         "Minimum value for uniform distribution, in ns, in addition to "
+         "link delay",
+         "0"},
+        {"max",
+         "Maximum value for uniform distribution, in ns, in addition to "
+         "link delay",
+         "10"})
 
     double min, max;
 
-  #ifdef ENABLE_SSTCHECKPOINT
+#ifdef ENABLE_SSTCHECKPOINT
     ImplementSerializable(UniformNode)
-  #endif
+#endif
 
 };
+
 #endif
